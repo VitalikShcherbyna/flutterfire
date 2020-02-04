70fa93882c246621b7264364b9c4c9c4c5aa2f40
Vitalii Shcherbyna <vitaliishcherbyna@Vitaliis-MacBook-Pro.local>
2020-02-04 11:15:24 +0100

    notification id

--- packages/firebase_messaging/android/src/main/java/io/flutter/plugins/firebasemessaging/NotificationReceiver.java
+++ packages/firebase_messaging/android/src/main/java/io/flutter/plugins/firebasemessaging/NotificationReceiver.java
@@ -32,6 +32,6 @@ public class NotificationReceiver extends BroadcastReceiver {
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context, FlutterFirebaseMessagingService.CHANNEL_ID)
         .setContentTitle("You have approved the Request");
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-            notificationManager.notify(MainActivity.NOTIFICATION_ID, mBuilder.build());
+            notificationManager.notify(FlutterFirebaseMessagingService.NOTIFICATION_ID, mBuilder.build());
     }
 }
